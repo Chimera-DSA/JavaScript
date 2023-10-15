aeb4b34ded88db742e0d2c174479b96f8b36ccb9
Scoheart <Scoheart1487790559@gmail.com>
2023-10-15 23:59:34 +0800

    feat: add new code

--- src/data-structure/linked-list/index.js
+++ src/data-structure/linked-list/index.js
@@ -1,7 +1,44 @@
+class LNode {
+  data;
+  next;
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-const linkedList = new LinkedList();
-linkedList.prepend(1);
-linkedList.prepend(3);
-linkedList.prepend(5);
+const head = new LNode();
+const Node1 = new LNode(1);
+const Node2 = new LNode(2);
+const Node3 = new LNode(3);
+const Node4 = new LNode(4);
+const Node5 = new LNode(5);
 
-console.log(linkedList);
+head.next = Node1;
+Node1.next = Node2;
+Node2.next = Node3;
+Node3.next = Node4;
+Node4.next = Node5;
+
+function re(root) {
+  let head = root.next;
+  let nums = 1;
+  while (head != null && nums !== 2) {
+    // console.log(temp.data)
+    head = head.next;
+    nums++;
+  }
+  console.log(head.data)
+}
+
+function re2(root) {
+  let nums = 1;
+  while (root !== null && nums !== 2) {
+    root = root.next;
+    nums++;
+  }
+  console.log(root.data);
+}
+
+re(head);
+// re2(Node1);
